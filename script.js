3736628a94022f3b952a505b196e20a66f797d07
Matt Vega <matthewvega301@gmail.com>
2022-08-26 19:54:08 -0400

    Adding initial files

---
+++ script.js
@@ -0,0 +1,105 @@
+
+let data = [
+    {
+      "courseId": 1,
+      "courseName": "Calculus",
+      "required": true,
+      "credit": 3
+    },
+    {
+      "courseId": 2,
+      "courseName": "Data Structures",
+      "required": true,
+      "credit": 3
+    },
+    {
+      "courseId": 3,
+      "courseName": "System Design",
+      "required": true,
+      "credit": 3
+    },
+    {
+      "courseId": 4,
+      "courseName": "Cyber Security",
+      "required": false,
+      "credit": 3
+    },
+    {
+      "courseId": 5,
+      "courseName": "Web Design",
+      "required": false,
+      "credit": 2
+    },
+    {
+      "courseId": 6,
+      "courseName": "Mobile Application Development",
+      "required": true,
+      "credit": 3
+    },
+    {
+      "courseId": 7,
+      "courseName": "Cryptography",
+      "required": false,
+      "credit": 1
+    },
+    {
+      "courseId": 8,
+      "courseName": "Block Chain Fundamentals",
+      "required": true,
+      "credit": 2
+    },
+    {
+      "courseId": 9,
+      "courseName": "GUI Design",
+      "required": false,
+      "credit": 2
+    },
+    {
+      "courseId": 10,
+      "courseName": "Game Design",
+      "required": false,
+      "credit": 3
+    },
+    {
+      "courseId": 11,
+      "courseName": "Intro to Archaetecture",
+      "required": false,
+      "credit": 3
+    },
+    {
+      "courseId": 12,
+      "courseName": "Intro to Cloud",
+      "required": false,
+      "credit": 2
+    },
+    {
+      "courseId": 13,
+      "courseName": "Computer Science Capstone I",
+      "required": true,
+      "credit": 3
+    },
+    {
+      "courseId": 14,
+      "courseName": "Computer Science Capstone II",
+      "required": true,
+      "credit": 3
+    },
+    {
+      "courseId": 15,
+      "courseName": "Computer Science Capstone III",
+      "required": true,
+      "credit": 3
+    }
+  ]
+
+function getData() {
+    // fetch('http://localhost:4232/courseList').then((response) => response.status).catch(error => console.log(error));
+}
+
+// getting a bunch of super weird errors with fetch and the XMLHTTP cant fetch data correctly, tried many things :(((
+
+function targetLists(){
+    let outerUl = document.getElementsById("outer-list");
+}
+
+targetLists();
